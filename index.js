78082c48b01f153fb330ebb587f19f561e983476
cfarn <58476975+cfarn@users.noreply.github.com>
2022-06-20 17:11:01 +0200

    suite

--- index.js
+++ index.js
@@ -2,14 +2,18 @@
 // const integer = 102
 // const float = 13.9
 // console.log(`${integer} ${float}`)
+
 // 02 - Convert
 // const basic = 34
 // const stringified = basic.toString()
 // console.log(stringified)
+// console.log(typeof stringified)
+
 // 03 - Round
 // const num = 1.5
 // const rounded = Math.round(num)
 // console.log(rounded)
+
 // 04 - Arithmétique
 // const test = 12
 // const bis = 5
@@ -18,7 +22,10 @@
 // console.log(test*bis)
 // console.log(test/bis)
 // console.log(test%bis)
+
 // 05 - Reste division euclidienne
 const val = [1,2,3,4,5]
 const modulo =999
 console.log(modulo%val)
+
+console.log()
